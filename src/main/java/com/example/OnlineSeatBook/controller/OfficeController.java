91c5991f5e3ed74a3b67608d568699e1bebe276f
SmaranKar <smaran20022002@gmail.com>
2024-04-24 14:46:44 +0530

    Updated Office Controller

--- src/main/java/com/example/OnlineSeatBook/controller/OfficeController.java
+++ src/main/java/com/example/OnlineSeatBook/controller/OfficeController.java
@@ -58,7 +58,7 @@ public class OfficeController {
         officeRepository.deleteById(id);
     }
     @CrossOrigin
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+//    @PreAuthorize("hasAnyAuthority('ROLE_USER','ROLE_ADMIN')")
     @GetMapping("/{officeId}/getFloors")
     public List<FloorDTO> getAllFloors(@PathVariable int officeId) {
         return officeService.getAllFloors(officeId);
